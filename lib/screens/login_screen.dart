9140cba921082e57a8305334a40e32802359f469
Jacob <jacobismael6@gmail.com>
2025-09-04 10:46:22 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,15 +9,65 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+   bool obscured = true;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    //para obtener el tamaño de la pantalla
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
       body: SafeArea(
+        //margen interior
+          child: Padding(
+            //
+        padding: const EdgeInsets.symmetric(horizontal: 20,),
         child: Column(
           children: [
-            Expanded(child: RiveAnimation.asset('asset/animated_login_character.riv'))
+            SizedBox(
+                width: size.width,
+                height: 200,
+                child:
+                    RiveAnimation.asset('asset/animated_login_character.riv')),
+            const SizedBox(height: 10),
+            //campo de texto de Email
+            TextField(
+              //para que aparezca @ en móviles
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: "E-Mail",
+                  prefixIcon: const Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              //para que aparezca password en móviles
+              obscureText: obscured,
+              decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscured = !obscured;
+                        
+                        
+                      }
+                      );
+                    },
+                    icon: Icon(
+                      
+                      obscured ? Icons.visibility : Icons.visibility_off
+                    )
+                    ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+              
+            )
           ],
-        )),
+        ),
+      )),
     );
   }
 }
