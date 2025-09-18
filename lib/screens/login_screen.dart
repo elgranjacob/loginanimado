f63be44aee36c39ee9e473c80667482199e5a35a
Jacob <jacobismael6@gmail.com>
2025-09-18 11:08:03 -0600

    feat(rive): write up isChecking/isHandsUp inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,27 +9,63 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-   bool obscured = true;
+  bool obscured = true;
+  //cerebro de las animaciones (state machine)
+  StateMachineController? controller;
+  //SMI: State Machine Input
+  SMIBool? isChecking;
+  SMIBool? isHandsUp;
+  SMITrigger? trigSuccess;
+  SMITrigger? trigFail;
+
   @override
   Widget build(BuildContext context) {
-    //para obtener el tamaño de la pantalla
+    //para obtener/consultar el tamaño de la pantalla
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        //margen interior
+          //margen interior
           child: Padding(
-            //
-        padding: const EdgeInsets.symmetric(horizontal: 20,),
+        //
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
         child: Column(
           children: [
             SizedBox(
                 width: size.width,
                 height: 200,
                 child:
-                    RiveAnimation.asset('asset/animated_login_character.riv')),
+                    RiveAnimation.asset(
+                      'asset/animated_login_character.riv',
+                      stateMachines: ["Login Machine"],
+                      //al iniciarse
+                      onInit: (artboard){
+                        controller = StateMachineController.fromArtboard(
+                          artboard, 
+                          "Login Machine",
+                          );
+                          //verificar que inicio bien
+                          if (controller == null) return;
+                          artboard.addController(controller!);
+                          isChecking = controller!.findSMI("isChecking");
+                          isHandsUp = controller!.findSMI("isHandsUp");
+                          trigSuccess = controller!.findSMI("trigSuccess");
+                          trigFail = controller!.findSMI("trigFail");
+                      },
+                    )),
             const SizedBox(height: 10),
             //campo de texto de Email
             TextField(
+              onChanged: (value){
+                if (isHandsUp != null){
+                  //no tapar los ojos
+                  isHandsUp!.change(false);
+                }
+                if (isChecking == null) return;
+                //activa el modo chismoso
+                isChecking!.change(true);
+              },
               //para que aparezca @ en móviles
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
@@ -41,30 +77,82 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(height: 10),
             TextField(
+              onChanged: (value){
+                if (isHandsUp != null){
+                  //no tapar los ojos
+                  isHandsUp!.change(true);
+                }
+                if (isChecking == null) return;
+                //activa el modo chismoso
+                isChecking!.change(false);
+                 
+
+              },
               //para que aparezca password en móviles
               obscureText: obscured,
               decoration: InputDecoration(
                   labelText: "Password",
                   prefixIcon: const Icon(Icons.lock),
                   suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        obscured = !obscured;
-                        
-                        
-                      }
-                      );
-                    },
-                    icon: Icon(
-                      
-                      obscured ? Icons.visibility : Icons.visibility_off
-                    )
-                    ),
+                      onPressed: () {
+                        setState(() {
+                          obscured = !obscured;
+                        });
+                      },
+                      icon: Icon(
+                          obscured ? Icons.visibility : Icons.visibility_off)),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                   )),
-              
-            )
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              width: size.width,
+              child: const Text(
+                "Forgot password",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  decoration: TextDecoration.underline
+                ),
+              ),
+            ),
+            //boton de login
+            SizedBox( 
+              height: 10,
+            ),
+            MaterialButton(
+              minWidth: size.width,
+              height: 50,
+              color: Colors.purple,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)
+              ),
+              onPressed: (){
+                //TODO
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: size.width,
+                child: Row(
+                  children: [
+                    const Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Colors.black,
+                        //en negritas
+                        fontWeight: FontWeight.bold,
+                        //subrayado
+                        decoration: TextDecoration.underline
+                      ),
+                    ),
+                  ],
+                ),
+              )
           ],
         ),
       )),
